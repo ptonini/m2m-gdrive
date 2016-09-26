8fef94d91f1529f10d782bfc75887b4c60807c62
ptonini <pedro.tonini@hotmail.com>
2016-09-26 17:55:28 -0300

    c

--- m2m-gdrive.py
+++ m2m-gdrive.py
@@ -1,6 +1,7 @@
 import sys
 from pydrive.drive import GoogleDrive
 from pydrive.auth import GoogleAuth
+import traceback
 
 
 def get_child_obj(obj_type, drive, parent_id, obj_title):
@@ -9,6 +10,7 @@ def get_child_obj(obj_type, drive, parent_id, obj_title):
         obj_list = drive.ListFile({'q': '"' + parent_id + '" in parents and trashed=false'}).GetList()
     except Exception as e:
         print e
+        traceback.print_exc()
         sys.exit(1)
 
     if obj_title:
@@ -16,9 +18,7 @@ def get_child_obj(obj_type, drive, parent_id, obj_title):
         matched = list()
         for obj in obj_list:
             if obj['title'] == obj_title:
-                if obj['mimeType'] != 'application/vnd.google-apps.folder':
-                    matched.append(obj)
-                if obj_type == 'folder' and obj['mimeType'] == 'application/vnd.google-apps.folder':
+                if obj['mimeType'] != 'application/vnd.google-apps.folder' or obj_type == 'folder':
                     matched.append(obj)
     else:
         matched = obj_list
@@ -34,34 +34,40 @@ def get_child_obj(obj_type, drive, parent_id, obj_title):
 
 
 def main():
-    settings_file = 'settings.yaml'
-    http_timeout = 5
+    action = 'download'
+    settings_file = './settings.yaml'
+    http_timeout = 10
     package_path = 'repo/app2/3.0'
     package_name = None
     save_to = 'worker-0.0.1-SNAPSHOT-jar-with-dependencies.jar'
 
     gauth = GoogleAuth(settings_file=settings_file, http_timeout=http_timeout)
-    drive = GoogleDrive(gauth)
 
-    folder_list = package_path.split('/')
+    if action == 'auth':
+        gauth.LocalWebserverAuth()
 
-    folder_obj = get_child_obj('folder', drive, 'root', folder_list[0])
-    folder_list.pop(0)
+    elif action == 'download':
 
-    while len(folder_list) > 0:
-        folder_obj = get_child_obj('folder', drive, folder_obj['id'], folder_list[0])
+        drive = GoogleDrive(gauth)
+        folder_list = package_path.split('/')
+
+        folder_obj = get_child_obj('folder', drive, 'root', folder_list[0])
         folder_list.pop(0)
 
-    file_obj = get_child_obj('file', drive, folder_obj['id'], package_name)
-    package = drive.CreateFile({'id': file_obj['id'], 'mimeType': file_obj['mimeType']})
+        while len(folder_list) > 0:
+            folder_obj = get_child_obj('folder', drive, folder_obj['id'], folder_list[0])
+            folder_list.pop(0)
 
-    try:
-        package.GetContentFile(save_to)
-    except Exception as e:
-        print e
-        sys.exit(1)
-    else:
-        print file_obj['title']
+        file_obj = get_child_obj('file', drive, folder_obj['id'], package_name)
+        package = drive.CreateFile({'id': file_obj['id'], 'mimeType': file_obj['mimeType']})
+
+        try:
+            package.GetContentFile(save_to)
+        except Exception as e:
+            print e
+            sys.exit(1)
+        else:
+            print file_obj['title']
 
 if __name__ == '__main__':
     main()
